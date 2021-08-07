a5d8cd6372e929c5aebbb418d5f0f942de105e16
SerjMartin <serjmartin@mail.com>
2021-08-07 13:03:13 +0000

    Create get_parsels_number function to get parcels quantity from central depo

--- run.py
+++ run.py
@@ -14,9 +14,18 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('delivery_company')
+#  These settings are to access spreadsheet data.
 
-parcels = SHEET.worksheet('parcels')
 
-data = parcels.get_all_values()
+def get_parcels_number():
+    """
+     Get parcels numbers from central depo.
+    """
+    print("Please enter the parcels number	expected for tomorrow")
+    print("The number should be > 10 and <= 1000\n")
 
-print(data)
+    data_str = input("Enter your data here:")
+    print(f"The numbers of parsels is {data_str}")
+
+
+get_parcels_number()
