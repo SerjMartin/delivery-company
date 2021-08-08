2fa2d0e68e26b89759c7ed2d789e66cd085038e9
SerjMartin <serjmartin@mail.com>
2021-08-08 21:57:38 +0000

    Add update_profit_worksheet function and pas data to profit worksheet

--- run.py
+++ run.py
@@ -65,7 +65,7 @@ def parcels_per_driver(parcels):
     headings = SHEET.worksheet("parcels").get_all_values()[0]
     # print(headings)
     per_day_parcels = round(int(parcels) / int(len(headings)))
-    print(f"{per_day_parcels} parcels per driver")
+    print(f"Every driver got per day:{per_day_parcels} parcels.")
 
     row = []
     for x in range(len(headings)):
@@ -94,7 +94,7 @@ def get_hours(parcels):
     hours_per_day = round(int(parcels) / int(len(headings)) / 30) + 1
     # One driver can do 30 parcel per hour + 1 hour for break
 
-    print(f"{hours_per_day} hour per day")
+    print(f"Every driver worked:{hours_per_day} hour.")
 
     row = []
     for x in range(len(headings)):
@@ -106,8 +106,7 @@ def get_hours(parcels):
 
 def update_hours_worksheet(hours):
     """
-     Update hours worksheet, add a new row with the number
-      of parcels per driver
+     Update hours worksheet, add a new row with the hours.
     """
     print("Updating hours worksheet...\n")
     hours_worksheet = SHEET.worksheet("hours")
@@ -123,7 +122,7 @@ def get_salary(hours):
     salary_per_day = round(int(parcels) / int(len(headings)) / 30) * 12
     # Driver get 12 € per hour
 
-    print(f"{salary_per_day} euro per day")
+    print(f"For every driver you pay:{salary_per_day} euro")
 
     row = []
     for x in range(len(headings)):
@@ -141,7 +140,43 @@ def update_salary_worksheet(salary):
     print("Updating salary worksheet...\n")
     salary_worksheet = SHEET.worksheet("salary")
     salary_worksheet.append_row(salary)
-    print("Hours salary updated successfuly.\n")
+    print("Salary worksheet updated successfuly.\n")
+
+
+def get_profit(hours):
+    """
+     Calculate full payment for all drivers
+     and your profit
+    """
+    headings = SHEET.worksheet("salary").get_all_values()[0]
+    salary_per_day = round(int(parcels) / int(len(headings)) / 30) * 12
+    # Driver get 12 € per hour
+
+    full_paymant = salary_per_day * len(headings)
+    print(f"You paid to drivers:{full_paymant} euro.")
+    # print(full_paymant)
+    full_profit = int(parcels) * 10 - int(full_paymant)
+    # Delivery company charging 10 € per parcel for service
+    # print(full_profit)
+    print(f"Your profit is: {full_profit} euro.")
+    headings = SHEET.worksheet("profit").get_all_values()[0]
+    row = []
+    for x in range(len(headings)):
+        x = full_profit
+        row.append(x)
+
+    return row
+
+
+def update_profit_worksheet(profit):
+    """
+     Update profit worksheet, add a new row with the profit.
+    """
+
+    print("Updating profit worksheet...\n")
+    profit_worksheet = SHEET.worksheet("profit")
+    profit_worksheet.append_row(profit)
+    print("Profit worksheet updated successfuly.\n")
 
 
 parcels = get_parcels_number()
@@ -151,3 +186,5 @@ hours = get_hours(parcels)
 update_hours_worksheet(hours)
 salary = get_salary(hours)
 update_salary_worksheet(salary)
+profit = get_profit(hours)
+update_profit_worksheet(profit)
